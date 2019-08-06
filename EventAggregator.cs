c6c9f7e6f6afe069fbd92bd0a9498a23c74186ad
GrzegorzKedzierski1999 <kedzierskigrzegorz1999@gmail.com>
2019-08-06 14:46:56 +0200

    Add files via upload

---
+++ EventAggregator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventAggregator
+{
+    public static class EventAggregator
+    {
+        private static Dictionary<Type, object> _events = new Dictionary<Type, object>();
+
+        public static T GetEvent<T>() where T : new()
+        {
+            if (_events.ContainsKey(typeof(T)))
+            {
+                return (T)_events[typeof(T)];
+            }
+            else
+            {
+                var aggregatedEvent = new T();
+                _events.Add(typeof(T), aggregatedEvent);
+                return aggregatedEvent;
+            }
+        }
+    }
+}
---
+++ PubSubEvent.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventAggregator
+{
+    public abstract class PubSubEvent<TPayload>
+    {
+        private TPayload _tPayload;
+
+        private List<Action<object>> _subscribers = new List<Action<object>>();
+
+        public void Publish(TPayload tPayload)
+        {
+            _tPayload = tPayload;
+            Execute(tPayload);
+        }
+
+        public void Subscribe(Action<object> action)
+        {
+            if (_subscribers == null)
+                _subscribers = new List<Action<object>>();
+
+            _subscribers.Add(action);
+            if (_tPayload != null)
+                action.Invoke(_tPayload);
+        }
+
+        private void Execute(object o)
+        {
+            foreach (var subscriber in _subscribers)
+                subscriber.Invoke(o);
+        }
+    }
+}
